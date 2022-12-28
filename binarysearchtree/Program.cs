fd636d56012620f0af84480c3fbcbfd48f775918
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-12-28 17:35:05 +0700

    add method

--- binarysearchtree/Program.cs
+++ binarysearchtree/Program.cs
@@ -45,8 +45,81 @@ namespace binarysearchtree
                     currentNode = currentNode.rightchild;
             }
         }
+        public void insert(string element)/* Insert a node in the binary search tree */
+        {
+            Node tmp, parent = null, currentNode = null;
+            search(element, ref parent, ref currentNode);
+            if (currentNode != null)/*Check if the node to be inserted already
+                                     * inserted or not*/
+            {
+                Console.WriteLine("Duplilcate words not allowed");
+                return;
+            }
+            else/* if the specified node is not present*/
+            {
+                tmp = new Node(element, null, null);/*creates a Node*/
+                if (parent == null) /* if the trees is empty*/
+                {
+                    ROOT = tmp;
+                }
+                else if (string.Compare(element, parent.info) < 0)
+                {
+                    parent.leftchild = tmp;
+                }
+                else
+                {
+                    parent.rightchild = tmp;
+                }
+            }
+        }
+
+        public void inorder(Node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                inorder(ptr.leftchild);
+                Console.WriteLine(ptr.info + "");
+                inorder(ptr.rightchild);
+            }
+        }
+
+        public void preorder (Node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is Empty");
+                return ;
+            }
+            if (ptr != null)
+            {
+                Console.WriteLine(ptr.info + "");
+                preorder(ptr.leftchild);
+                preorder(ptr.rightchild);
+            }
+        }
+        public void postorder(Node ptr)/* Performs the postorder traversal
+                                        * of the three*/
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is Empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                postorder(ptr.leftchild);
+                postorder(ptr.rightchild);
+                Console.WriteLine(ptr.info + "");
+            }
+        }
         static void Main(string[] args)
         {
+            
         }
     }
 }
