3a967a6822ebe7f941355241cdfd41099a7464d1
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-12-28 17:18:09 +0700

    add method in class program

--- binarysearchtree/Program.cs
+++ binarysearchtree/Program.cs
@@ -6,16 +6,45 @@ using System.Threading.Tasks;
 
 namespace binarysearchtree
 {
-    class node
+    class Node
     {
         public string info;
-        public string leftchild;
-        public string rightchild;
+        public Node leftchild;
+        public Node rightchild;
 
         //Constructor for the node class
+
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
     }
-    internal class Program
+
+    /*A node class consists of three things, the information,
+     * references to the right child, and refrences to the left child*/
+    class Program
     {
+        public Node ROOT;
+        public Program()
+        {
+            ROOT = null;/*Initializing ROOT to null*/
+        }
+        public void search(string element, ref Node perent, ref Node currentNode)
+        {
+            /*This function searchs the currentNode of the specified Node as well as the current Node of its perents */
+            currentNode = ROOT;
+            perent = null;
+            while((currentNode != null) && (currentNode.info != element))
+            {
+                perent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else
+                    currentNode = currentNode.rightchild;
+            }
+        }
         static void Main(string[] args)
         {
         }
